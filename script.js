77d4e819cdf5ee1a8329725fd9456e2528b42f8a
rishabhy043 <rishabh.knipss043@gmail.com>
2024-11-11 22:10:54 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,55 @@
-let userScore = 0;
-let compScore = 0;
+let userscore = 0;
+let compscore = 0;
+
 const choices = document.querySelectorAll(".choice");
-const msg = document.querySelector("#msg");
-const userScorePara = document.querySelector("#user-score");
-const compScorePara = document.querySelector("#comp-score");
+
+
+
+// draw game function
+const drawgame = () => {
+console.log("the game is draw")
+}
+
+const showwinnner = (userwin) => {
+if(userwin){
+    console.log("you wins!")
+}else{
+    console.log("you lose!")
+}
+}
+// generates cmputer choices
+const gencompchoice = () =>{
+const options = ["Rock" , "paper" , "Scissor"];
+const randidx =  Math.floor(Math.random()*3);
+return options[randidx]
+}
+
+const playgame = (userchoice) =>{
+    // pass user choice and pass it 
+    console.log("user choice is :" , userchoice);
+    // pass computer choice and prints it
+const  compchoice =  gencompchoice();
+console.log("comptuer choice is :" , compchoice);
+if(userchoice == compchoice){
+    drawgame();
+}else{
+    let userwin = true;
+    if( userchoice == "rock" ){
+     userwin = compchoice =="paper" ? false:true;
+    }
+    else if( userchoice = "paper"){
+        userwin = compchoice == "scissor" ? false : true;
+    }
+    else{
+        userwin = compchoice == "rock"?false :true;
+    }
+    showwinnner(userwin);
+}
+};
+
+choices.forEach((choice) => {
+choice.addEventListener("click" , () =>{
+    const userchoice = choice.getAttribute("id")
+playgame(userchoice)
+});
+});
